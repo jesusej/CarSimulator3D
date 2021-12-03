ad44fa6c0b9bdfe6356d63fa0eae80ce50a2d48c
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2021-12-03 15:11:46 -0700

    :bug: Deleting array for Unity

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ port = int(os.getenv('PORT', 8000))
 
 @app.route('/')
 def root():
-    return jsonify([createJson()])
+    return jsonify(createJson())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
